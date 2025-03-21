b078972e44aaa183995c48a8b894bfd65334f07a
msDIZON <jasminedizon0728@gmail.com>
2025-03-21 21:31:51 +0800

    Create colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,8 @@
+let my_favColors = [];
+let limit = 3
+
+for (let i = 0; i < limit; i++) {
+    let add_color = prompt("Enter another color you like:");
+    my_favColors.push(add_color); 
+    console.log("Updated List of Your Favorite Colors:", my_favColors);
+}
