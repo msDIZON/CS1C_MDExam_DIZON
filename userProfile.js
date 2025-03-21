9e7ebb6f6a3e068f24381573fbdffd9885274107
msDIZON <jasminedizon0728@gmail.com>
2025-03-21 21:28:10 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let full_name = prompt("Please enter your Full Name is:");
+let age = prompt("Enter your age:");
+let fav_num = prompt("Please enter your Favorite Number is:");
+let fav_color = prompt("Please enter your Favorite Colors is:");
+
+console.log("Your Full Name is:", full_name);
+console log("Your age is:", age);
+console.log("Your Favorite Number is:", fav_num);
+console log("YourFavorite Colors is:", fav_color);
