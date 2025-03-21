77eb3c6adbf15348581afd6546b2a5b34cac4684
msDIZON <jasminedizon0728@gmail.com>
2025-03-21 22:09:10 +0800

    Update groceryList.js

--- groceryList.js
+++ groceryList.js
@@ -5,13 +5,11 @@ let addItems = prompt("Do you want to create a grocery list? (y/n):");
 if (addItems.toLowerCase() === 'y') {
     let numberOfItems = parseInt(prompt("How many items do you want to register?"));
 
-    
     for (let i = 0; i < numberOfItems; i++) {
         let item = prompt("Enter grocery item " + (i + 1) + ":");
         groceryList.push(item); 
     }
 
-  
     let groceryList_Sort = [...groceryList].sort();  
     let groceryList_Reverse = [...groceryList_Sort].reverse(); 
     
