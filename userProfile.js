cccdfefb35f15a2e24d74fd6d96153d3056beba5
msDIZON <jasminedizon0728@gmail.com>
2025-03-24 08:01:51 +0800

    Update userProfile.js

--- userProfile.js
+++ userProfile.js
@@ -4,6 +4,6 @@ let fav_num = prompt("Please enter your Favorite Number is:");
 let fav_color = prompt("Please enter your Favorite Colors is:");
 
 console.log("Your Full Name is:", full_name);
-console log("Your age is:", age);
+console.log("Your age is:", age);
 console.log("Your Favorite Number is:", fav_num);
-console log("YourFavorite Colors is:", fav_color);
+console.log("YourFavorite Colors is:", fav_color);
